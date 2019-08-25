1cd41bfb6132a116e9f78c8de3d2c1f62f6bebc3
李繁 <fan.lion@foxmail.com>
2019-08-25 13:44:33 +0800

    feat: 添加基础module
    新增login、about等module

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,11 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { AboutModule } from './about/about.module';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterModule)
+  },
+  {
+    path: 'rank',
+    loadChildren: () => import('./rank/rank.module').then(m => m.RankModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
   }
 ];
 
