03e47e676502bed998d6064fa3137d16293f617e
李繁 <fan.lion@foxmail.com>
2019-08-25 19:05:03 +0800

    feat: 添加pace.js
    添加pace.js 启动动画

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ElectronService, EngineService } from './core/services';
+import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from '../environments/environment';
 
@@ -11,18 +11,12 @@ import { AppConfig } from '../environments/environment';
 export class AppComponent {
   constructor(
     public electronService: ElectronService,
-    public engineService: EngineService,
     private translate: TranslateService
   ) {
     translate.setDefaultLang('en');
-    engineService.sayHello();
-    console.log('AppConfig', AppConfig);
-
     if (electronService.isElectron) {
       console.log(process.env);
       console.log('Mode electron');
-      console.log('Electron ipcRenderer', electronService.ipcRenderer);
-      console.log('NodeJS childProcess', electronService.childProcess);
     } else {
       console.log('Mode web');
     }
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -11,7 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: DashboardComponent
   },
   {
